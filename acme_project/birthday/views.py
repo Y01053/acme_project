791cac88f9ea980b8c2d7d35d333b23f8572d825
Иван Ширинкин <vanosh25@yandex.ru>
2023-07-25 16:39:09 +0300

    First commit

--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -1,6 +1,28 @@
+# birthday/views.py 
 from django.shortcuts import render
+
+from .forms import BirthdayForm
+# Импортируем из utils.py функцию для подсчёта дней.
+from .utils import calculate_birthday_countdown
 
 
 def birthday(request):
-    context = {}
-    return render(request, 'birthday/birthday.html', context=context)
+    form = BirthdayForm(request.GET or None)
+    # Создаём словарь контекста сразу после инициализации формы.
+    context = {'form': form}
+    # Если форма валидна...
+    if form.is_valid():
+        # ...вызовем функцию подсчёта дней:
+        birthday_countdown = calculate_birthday_countdown(
+            # ...и передаём в неё дату из словаря cleaned_data.
+            form.cleaned_data['birthday']
+        )
+        # Обновляем словарь контекста: добавляем в него новый элемент.
+        context.update({'birthday_countdown': birthday_countdown})
+    return render(request, 'birthday/birthday.html', context)
+
+    """Его логика такова: если в GET-запросе были переданы параметры — 
+    значит, объект request.GET не пуст и этот объект передаётся в форму;
+    если же объект request.GET пуст — 
+    срабатывает условиe or и форма создаётся без параметров,
+    через BirthdayForm(None) — это идентично обычному BirthdayForm()."""
